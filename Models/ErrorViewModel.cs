80fcf7e3f466f718d2cd3755fd3e7bfd9a21ec62
PainnIrene <hoanghuytran10102003@gmail.com>
2023-11-21 01:08:12 +0700

    init project mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace App_Dev_1670.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
